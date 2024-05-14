b686497aed3013045a085b8b7df4281ea690ae05
micheal soliman <micheal1242002@gmail.com>
2024-05-14 16:31:51 +0300

    creet

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './header/Header.css'
+const Header = () => {
+  return (
+    <div>
+      <h1>lrfkergntnktjgnjkr</h1>
+    </div>
+  )
+}
+
+export default Header
